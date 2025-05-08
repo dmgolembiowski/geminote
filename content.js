81aba7cf6aa37d9edc4aa8dc6655e23529b4c4e2
dmgolembiowski <dmgolembiowski@gmail.com>
2025-05-08 05:37:07 -0400

    Fixed directory traversal

--- content.js
+++ content.js
@@ -2,6 +2,7 @@ document.addEventListener('click', (event) => {
   const target = event.target;
   if (target.tagName === 'A' && target.href.startsWith('file:///')) {
     event.preventDefault(); // Prevent the default download behavior
+    if (target.href.endsWith('/')) { return; }
     const filePath = target.href;
     chrome.runtime.sendMessage({ action: "readFile", path: filePath });
   }
